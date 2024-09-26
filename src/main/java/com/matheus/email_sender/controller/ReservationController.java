5611f125ad5af29dfcdde3098dcc09e3472d54ea
Matheus <cristo.matheus@outlook.com>
2024-09-26 12:18:05 -0300

    feat: Add ReservationController

---
+++ src/main/java/com/matheus/email_sender/controller/ReservationController.java
@@ -0,0 +1,30 @@
+package com.matheus.email_sender.controller;
+
+import com.matheus.email_sender.dto.reservation.ReservationCreateDto;
+import com.matheus.email_sender.model.Hotel;
+import com.matheus.email_sender.model.Reservation;
+import com.matheus.email_sender.service.ReservationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/reservation")
+public class ReservationController {
+
+    @Autowired
+    private ReservationService reservationService;
+
+    @GetMapping
+    public ResponseEntity<Reservation> findReservationById(@RequestParam String id){
+        return ResponseEntity.ok().body(reservationService.findReservationById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Reservation> createReservation(@RequestBody ReservationCreateDto reservationCreateDto){
+        return ResponseEntity.ok().body(reservationService.createReservation(reservationCreateDto));
+    }
+
+}
+
+
