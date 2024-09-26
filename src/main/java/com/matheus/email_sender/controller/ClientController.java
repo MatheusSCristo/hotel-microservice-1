08001998580414c12dbaa8847c9c3caf5d7b9cae
Matheus <cristo.matheus@outlook.com>
2024-09-26 12:12:44 -0300

    feat: Add ClientController

---
+++ src/main/java/com/matheus/email_sender/controller/ClientController.java
@@ -0,0 +1,30 @@
+package com.matheus.email_sender.controller;
+
+import com.matheus.email_sender.dto.client.ClientCreateDto;
+import com.matheus.email_sender.model.Client;
+import com.matheus.email_sender.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/client")
+public class ClientController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @GetMapping
+    public ResponseEntity<Client> findClientById(@RequestParam String id) {
+        return ResponseEntity.ok().body(clientService.findClientById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Client> createClient(ClientCreateDto clientCreateDto) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(clientService.createClient(clientCreateDto));
+    }
+
+}
